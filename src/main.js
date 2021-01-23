8fcd167b69056af34e84a4a45fcb31c18394ecbf
李林峰 <875770918@qq.com>
2021-01-23 15:01:43 +0800

    1

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+//加载全局样式文件
+import './styles/index.css'
 Vue.config.productionTip = false
 
+Vue.use(ElementUI);
 new Vue({
-  render: h => h(App),
+  router,
+  render: h => h(App)
 }).$mount('#app')
