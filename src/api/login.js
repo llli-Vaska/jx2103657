4a3766837b7d96f9255e99181361261e33aa1514
李林峰 <875770918@qq.com>
2021-01-26 15:49:06 +0800

    2021/1/25 21:04

--- src/api/login.js
+++ src/api/login.js
@@ -1,10 +1,9 @@
 import axios from "axios";
 
-function login(params){
+export function login(params){
     return axios({
         url:'/adminlogin',
         method:'post',
         params: params
     })
 }
-module.exports = login
