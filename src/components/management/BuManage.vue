81e0d15f6b483086aef23828ce0bae483e3de542
李林峰 <875770918@qq.com>
2021-02-04 17:16:48 +0800

    完成后台企业管理页面的页面布局

--- src/components/management/BuManage.vue
+++ src/components/management/BuManage.vue
@@ -1,11 +1,162 @@
 <!--企业管理-->
 <template>
-  <div>
+  <div class="bu">
     <el-breadcrumb separator-class="el-icon-arrow-right">
       <el-breadcrumb-item :to="{ path: '/admin' }">首页</el-breadcrumb-item>
       <el-breadcrumb-item>用户管理</el-breadcrumb-item>
       <el-breadcrumb-item>企业管理</el-breadcrumb-item>
     </el-breadcrumb>
+
+    <el-form>
+      <el-form-item label="搜索:" class="query-form">
+        <el-input v-model="bu_name" placeholder="输入公司名" class="bu-query-name"></el-input>
+        <el-button type="primary" icon="el-icon-search" class="btn-query" @click="searchname">搜索</el-button>
+        <el-button type="primary" icon="el-icon-plus" class="btn-query" @click="dialogFormVisible = true">添加</el-button>
+
+        <el-dialog title="添加用户" :visible.sync="dialogFormVisible">
+          <el-form :model="form" >
+            <el-form-item label="公司名:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.CompanyName" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="用户名(账号):" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.UserName" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="密码:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.UserPassword" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="法人代表:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.CompanyPerson" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="手机号:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.Phone" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.Email" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="公司地址:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.CompanyAddress" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="公司类型:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.CompanyType" autocomplete="off"></el-input>
+            </el-form-item>
+            <el-form-item label="公司规模:" :label-width="formLabelWidth" class="dialog-form">
+              <el-input v-model="form.CompanyScale" autocomplete="off"></el-input>
+            </el-form-item>
+          </el-form>
+          <div slot="footer" class="dialog-footer">
+            <el-button @click="dialogFormVisible = false">取 消</el-button>
+            <el-button type="primary" @click="AddCompany()">确 定</el-button>
+          </div>
+        </el-dialog>
+
+        <el-button type="danger" icon="el-icon-delete" class="btn-query" @click="BubatchDelete(tableChecked)">批量删除</el-button>
+      </el-form-item>
+
+
+    </el-form>
+
+
+    <el-table
+        ref="multipleTable"
+        :data="tableData"
+        tooltip-effect="dark"
+        style="width: 100%"
+        :default-sort = "{prop: 'date', order: 'descending'}"
+        border
+        @selection-change="handleSelectionChange">
+      <el-table-column
+          type="selection"
+          width="50">
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="CompanyName"
+          label="公司名"
+          width="150"
+          show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="UserName"
+          label="用户(账号)"
+          width="120">
+        <template slot-scope="scope">{{ scope.row.UserName }}</template>
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="UserPassword"
+          label="密码"
+          width="120">
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="CompanyPerson"
+          label="法人代表"
+          width="110">
+      </el-table-column>
+      <el-table-column
+          prop="Phone"
+          label="手机号"
+          width="120"
+          show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="Email"
+          label="邮箱"
+          width="120"
+          show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="CompanyAddress"
+          label="公司地址"
+          width="150"
+          show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="CompanyType"
+          label="公司类型"
+          width="120"
+          show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column
+          sortable
+          prop="CompanyScale"
+          label="公司规模"
+          width="120"
+          show-overflow-tooltip>
+      </el-table-column>
+      <el-table-column label="操作" >
+        <template slot-scope="scope">
+          <el-button
+              style="margin-right: 10px"
+              size="mini"
+              @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+
+
+          <el-button
+              size="mini"
+              type="danger"
+              @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+
+    </el-table>
+
+    <!--分页-->
+    <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pageNum"
+        :page-sizes="[15, 25, 35]"
+        :page-size="queryInfo.pageSize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
+    </el-pagination>
+
+
   </div>
 
 
@@ -13,10 +164,135 @@
 
 <script>
 export default {
-name: "BuManage"
+name: "BuManage",
+  data() {
+  return {
+    //分页
+    queryInfo: {
+      pageNum: 1,
+      pageSize: 15
+    },
+    total:0,
+
+
+
+    bu_name:'',
+    dialogFormVisible: false, //添加弹框
+    tableData:[ {
+      CompanyName: '123', //公司名
+      UserName: '123', //账号
+      UserPassword: '123', //密码
+      CompanyPerson: '123', //法人代表
+      Phone: '123', //手机号
+      Email: '123', //邮箱
+      CompanyAddress: '123', //公司地址
+      CompanyType: '123', //公司类型
+      CompanyScale: '123', //公司规模
+    },{
+      CompanyName: '123', //公司名
+      UserName: '123', //账号
+      UserPassword: '123', //密码
+      CompanyPerson: '123', //法人代表
+      Phone: '123', //手机号
+      Email: '123', //邮箱
+      CompanyAddress: '123', //公司地址
+      CompanyType: '123', //公司类型
+      CompanyScale: '123', //公司规模
+    }],
+    //添加表单
+    form: {
+      CompanyName: '', //公司名
+      UserName: '', //账号
+      UserPassword: '', //密码
+      CompanyPerson: '', //法人代表
+      Phone: '', //手机号
+      Email: '', //邮箱
+      CompanyAddress: '', //公司地址
+      CompanyType: '', //公司类型
+      CompanyScale: '', //公司规模
+    },
+//编辑 删除 暂存空间
+    row: {
+      CompanyName: '', //公司名
+      UserName: '', //账号
+      UserPassword: '', //密码
+      CompanyPerson: '', //法人代表
+      Phone: '', //手机号
+      Email: '', //邮箱
+      CompanyAddress: '', //公司地址
+      CompanyType: '', //公司类型
+      CompanyScale: '', //公司规模
+    },
+    formLabelWidth: '100px',
+    //选中
+    tableChecked:[],
+
+  }
+  },
+  methods: {
+  //添加确定
+    AddCompany() {
+      //关闭对话框
+      this.dialogFormVisible = false
+    },
+  //搜索功能
+    searchname() {
+      console.log(this.bu_name)
+    },
+  //批量删除
+    BubatchDelete(val) {
+      console.log(val)
+    },
+    //编辑修改公司信息
+    handleEdit(index, row) {
+      console.log(index, row);
+    },
+    //删除单条信息
+    handleDelete(index, row) {
+      console.log(index, row);
+    },
+    //选中的信息
+    handleSelectionChange(val) {val
+      //选中需要删除的信息
+      console.log()
+    },
+    //分页
+    //监听尺寸改变
+    handleSizeChange(newSize) {
+      console.log(newSize);
+
+    },
+    //监听页码改变
+    handleCurrentChange(newPage) {
+      console.log(newPage);
+
+    },
+
+
+  }
 }
 </script>
 
 <style scoped>
-
+.dialog-form {
+  width: 420px;
+  margin-bottom: 10px;
+}
+.bu {
+  margin: 24px 16px;
+  padding: 24px;
+  background: #fff;
+  min-height: 570px
+}
+.query-form {
+  display: flex;
+  margin-top: 20px;
+}
+.bu-query-name {
+  width: 300px;
+  padding-left: 10px;
+}
+.btn-query {
+  margin-left: 15px;
+}
 </style>
